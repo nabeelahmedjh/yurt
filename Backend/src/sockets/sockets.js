dc71f0a832351f91643663d390023062137180cf
Nabeel Ahmed <72853909+nabeelahmedjh@users.noreply.github.com>
2024-05-14 09:52:30 +0500

    removed socket io disconnect bug

--- Backend/src/sockets/sockets.js
+++ Backend/src/sockets/sockets.js
@@ -1,11 +1,11 @@
 class WebSockets {
   users = [];
   connection(client) {
-    console.log(client)
+    console.log(client);
     // event fired when the chat room is disconnected
-    client.on("disconnect", () => {
-      this.users = this.users.filter((user) => user.socketId !== client.id);
-    });
+    // client.on("disconnect", () => {
+    //   this.users = this.users.filter((user) => user.socketId !== client.id);
+    // });
     // add identity of user mapped to the socket id
     client.on("identity", (userId) => {
       this.users.push({
