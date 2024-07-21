7fc10c1b6983ae55a20f5ff5918c06ef7b83f033
Shah Zaib <shahzaib1234@outlook.com>
2024-07-21 18:52:57 +0500

    emit event on session start

--- Frontend/app/(main)/(chat)/servers/(routes)/layout.tsx
+++ Frontend/app/(main)/(chat)/servers/(routes)/layout.tsx
@@ -1,8 +1,32 @@
 "use client";
 import ChatLayout from "@/components/chat/chat-layout";
-import React from "react";
+import { useEffect, useState } from "react";
+
+import { socket } from "@/app/socket-client";
+import { getData } from "@/lib/get-data";
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
+  const [isMounted, setIsMounted] = useState(false);
+  const [profileId, setProfileId] = useState<any>(null);
+
+  useEffect(() => {
+    setIsMounted(true);
+
+    async function getProfile() {
+      const profileData = await getData("/auth/profile");
+      setProfileId(profileData.user._id);
+    }
+    getProfile();
+
+    if (isMounted && profileId) {
+      if (!sessionStorage.getItem("isIdentitySent")) {
+        socket.emit("identity", profileId);
+
+        sessionStorage.setItem("isIdentitySent", "true");
+      }
+    }
+  }, [isMounted, profileId]);
+
   return (
     <>
       <main className="h-[100dvh]">
