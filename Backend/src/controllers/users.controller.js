cd59ac704de999d08b1936cf6e4aef423b2eeebf
Zohaib Ahmad <SP21-BCS-048@STUDENTS.CUISAHIWAL.EDU.PK>
2024-08-24 19:01:48 +0500

    fixed status code for get All users

--- Backend/src/controllers/users.controller.js
+++ Backend/src/controllers/users.controller.js
@@ -9,9 +9,18 @@ const getAllUsers = async (req, res) => {
 
     try {
         const users = await usersService.getAllUsers(username, searchType)
+        
+        if (Array.isArray(users) && users.length === 0) {
+            return res.status(404).json({
+                error: { message: "No users found" },
+            });
+        }
+
         return res.status(200).json({
             data: users,
         });
+        
+        
     } catch (error) {
         return res.status(500).json({
 			error: { message: error.message },
