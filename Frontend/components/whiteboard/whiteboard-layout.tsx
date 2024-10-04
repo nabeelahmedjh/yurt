264ed1f00128119f8c82709ff4572001bf7dc014
Shahzaib <shahzaib1234@outlook.com>
2024-10-04 17:38:09 +0500

    possible filemanager path fix

--- Frontend/components/whiteboard/whiteboard-layout.tsx
+++ Frontend/components/whiteboard/whiteboard-layout.tsx
@@ -5,7 +5,7 @@ import useGetProfile from "@/hooks/user/useGetProfile";
 export default function WhiteboardLayout() {
   const { data: profile, isLoading } = useGetProfile();
 
-  if (isLoading) {
+  if (isLoading && !profile) {
     return (
       <div className="w-full h-full bg-secondary flex justify-center items-center animate-pulse">
         <Loader className="animate-spin" />
