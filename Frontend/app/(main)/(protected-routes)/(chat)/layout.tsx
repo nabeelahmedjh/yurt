2b3e84affc7e4c2d5aa3c0d9cb67005df9619717
Shahzaib <shahzaib1234@outlook.com>
2024-11-17 18:39:13 +0500

    improved user existence check based on backend

--- Frontend/app/(main)/(protected-routes)/(chat)/layout.tsx
+++ Frontend/app/(main)/(protected-routes)/(chat)/layout.tsx
@@ -2,14 +2,37 @@
 
 import ChatLayout from "@/components/chat/chat-layout";
 import ChatMobileLayout from "@/components/chat/chat-mobile-layout";
-
-import { emitIdentity } from "@/lib/emit-identity";
+import useGetProfile from "@/hooks/user/useGetProfile";
 import { useMediaQuery, useIsClient } from "usehooks-ts";
+import { useRouter } from "next/navigation";
+import { deleteCookie } from "cookies-next";
+import { TOKEN, USER_ID } from "@/constants";
 
 export default function Layout({ children }: { children?: React.ReactNode }) {
-  emitIdentity();
+  const router = useRouter();
   const isClient = useIsClient();
   const desktop = useMediaQuery("(min-width: 1024px)");
+
+  const { data, isLoading, error } = useGetProfile();
+  console.log(data);
+
+  if (isLoading) return "";
+
+  if (error) {
+    console.log(error);
+    deleteCookie(TOKEN);
+    deleteCookie(USER_ID);
+    router.refresh();
+    return "";
+  }
+
+  if (!isLoading && !data._id) {
+    console.log("User not found");
+    deleteCookie(TOKEN);
+    deleteCookie(USER_ID);
+    router.refresh();
+    return "";
+  }
 
   return (
     <>
